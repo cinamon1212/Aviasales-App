e7631663c6d30bfc0323450f34414332e03ea6bd
andrey <andrey.kuzmin.2806@mail.ru>
2023-05-12 15:26:35 +0300

    reduxDevtools

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { applyMiddleware, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import reduxThunk from 'redux-thunk';
 
 import { logger } from './middleWare/logger';
 import { rootReducer } from './redux/reducers/rootReducer';
 import { App } from './components/App/App';
 
-const store = createStore(rootReducer, applyMiddleware(logger, reduxThunk));
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger, reduxThunk)));
 
 const root = createRoot(document.getElementById('root'));
 root.render(
